9e1b58e793e48a8221ea3b5a979ecc16cd7222d9
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-05 17:12:54 +0700

    Sumbit solution 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (startNumber, endNumber) {
+  if (
+    startNumber < 0 ||
+    endNumber < 0 ||
+    typeof startNumber != "number" ||
+    typeof endNumber != "number"
+  ) {
+    return "ERROR";
+  }
+  if (startNumber > endNumber) {
+    [startNumber, endNumber] = [endNumber, startNumber];
+  }
+  return ((endNumber - startNumber + 1) / 2) * (startNumber + endNumber);
 };
 
 // Do not edit below this line

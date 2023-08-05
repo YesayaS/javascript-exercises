fed16d62f31da423d1a2d7c5e197f2f447f4eaef
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-05 16:32:56 +0700

    Submit solution 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...numbers) {
+  numbers.forEach((number) => {
+    index = inputArray.indexOf(number);
+    if (index >= 0) {
+      inputArray.splice(index, 1);
+    }
+  });
+  return inputArray;
 };
 
 // Do not edit below this line

105d73b8833693e31e9ef5b4b69c25fe5580b4ab
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-05 18:14:07 +0700

    Submit solution 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sumArray = 0;
+  array.forEach((num) => {
+    sumArray += num;
+  });
+  return sumArray;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let mulArray = 1;
+  array.forEach((num) => {
+    mulArray *= num;
+  });
+  return mulArray;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) {
+    return 1;
+  }
+  let fac = 1;
+  for (let i = num; i > 0; i--) {
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

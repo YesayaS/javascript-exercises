2b2012fe0d116a9fff9c0dcd645529f3573bb21f
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-05 15:32:56 +0700

    Submit solution 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (stringInput, repeat) {
+  if (repeat < 0) {
+    return "ERROR";
+  }
+  return stringInput.repeat(repeat);
 };
 
 // Do not edit below this line

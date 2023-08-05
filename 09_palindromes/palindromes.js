c012d5b554ac55b4d1f1dbf3016f0455f9b488cb
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-05 18:35:24 +0700

    Submit solution 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+  let punctuation = /[\.,?!]/g;
+  inputString = inputString
+    .replace(punctuation, "")
+    .replace(/\s/g, "")
+    .toLowerCase();
+  console.log(inputString);
+  return inputString === inputString.split("").reverse().join("");
 };
 
 // Do not edit below this line

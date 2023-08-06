05db94ecc2a6473def5f866bdd475a96dc4ceaf0
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-06 13:42:02 +0700

    Submit solution 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  parseInt(number);
+  if (number == 0 || number == 1) return 1;
+  f0 = 0;
+  f1 = 1;
+  for (let i = 1; i < number; i++) {
+    fib = f0 + f1;
+    f0 = f1;
+    f1 = fib;
+  }
+  return fib;
 };
 
 // Do not edit below this line
